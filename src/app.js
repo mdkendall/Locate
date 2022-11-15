1145f2e78363ce29629b07113d647d92676a14fe
Matthew Kendall <mdkendall@users.noreply.github.com>
2022-11-15 12:51:36 -0800

    Initial prototype

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            message: 'Locate',
+            parts: [],
+            uniqValues: [],
+            selectedValue: ""
+        }
+    },
+    methods: {
+        loadParts(partText) {
+            this.parts = $.csv.toObjects(partText);
+            for (part of this.parts) {
+                if (!this.uniqValues.includes(part.Description)) this.uniqValues.push(part.Description);
+            }
+        },
+        selectedParts() {
+            return this.parts.filter(part => {return part.Description == this.selectedValue;});
+        }
+    },
+    mounted() {
+        fetch('parts.csv').then(res => res.text()).then(text => this.loadParts(text));
+    }
+});
+
+app.mount('#app');
