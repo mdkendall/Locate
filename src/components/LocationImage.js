49b795f5fc4cb215663114cf77e0f1c273cb7abe
Matthew Kendall <mdkendall@users.noreply.github.com>
2022-12-09 15:36:16 -0800

    Scale markers relative to image size

--- src/components/LocationImage.js
+++ src/components/LocationImage.js
@@ -5,7 +5,7 @@ export default {
             <image :x="img.left" :y="img.bottom" :width="img.width" :height="img.height" :href="img.data" />
             <g :transform="'translate(0,'+(2*img.bottom+img.height).toString()+') scale(1,-1)'">
                 <g :transform="flip ? 'translate('+(2*img.left+img.width).toString()+',0) scale(-1,1)' : ''">
-                    <circle v-for="part in parts" :cx="part.X" :cy="part.Y" r="1" stroke="red" stroke-width="0.5" fill="yellow" opacity="0.5"/>
+                    <circle v-for="part in parts" :cx="part.X" :cy="part.Y" :r="Math.max(img.width,img.height)/80" stroke="red" :stroke-width="Math.max(img.width,img.height)/160" fill="yellow" opacity="0.5"/>
                 </g>
             </g>
         </svg>`
